cc537c5dcd8f72404b5619cfbb896a2eefc576a8
Dagnachew Woldeyohannes <d.b.woldeyohannes@gmail.com>
2020-10-09 05:33:42 -0700

    update file

--- app.java
+++ app.java
@@ -1,19 +1,31 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
-public class test2 {
+public class TestClass {
 
-public static void main (String[] args) {
+    static double ONE_INCH_IN_CM = 2.54;
+    static int ONE_FEET_IN_INCH = 12;
 
-ArrayList<String> rivers = new ArrayList<String>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input height in cm: ");
+        int heightInCm = scanner.nextInt();
+        double fullHeightInInch = cmToInch(heightInCm);
+        int feetHeight = calculateFeetHeight(fullHeightInInch);
+        int inchHeight = calculateInchHeight(fullHeightInInch);
+        String formatedOutput = "Height is: " + feetHeight + "\'" + inchHeight + "\"";
+        System.out.println(formatedOutput);
+    }
 
-rivers.add("Mississippi");
+    public static double cmToInch(int heightInCm){
+        double fullHeightInInch = heightInCm / ONE_INCH_IN_CM;
+        return fullHeightInInch;
+    }
+    public static int calculateFeetHeight(double fullHeightInInch) {
+        int feetHeight = (int) ((fullHeightInInch - fullHeightInInch % ONE_FEET_IN_INCH) / ONE_FEET_IN_INCH);
+        return feetHeight;
+    }
+    public static int calculateInchHeight(double fullHeightInInch) {
+        return (int) Math.round (fullHeightInInch % ONE_FEET_IN_INCH) ;
 
-rivers.add("Nile");
-
-rivers.add("Amazon");
-
-System.out.println(rivers);
-
-}
-
+    }
 }
