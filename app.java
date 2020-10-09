0e4aaad2ec7ed56ec73c5981974315ff7ae6381b
Dagnachew Woldeyohannes <d.b.woldeyohannes@gmail.com>
2020-10-09 05:14:31 -0700

    first commit

---
+++ app.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class test2 {
+
+public static void main (String[] args) {
+
+ArrayList<String> rivers = new ArrayList<String>();
+
+rivers.add("Mississippi");
+
+rivers.add("Nile");
+
+rivers.add("Amazon");
+
+System.out.println(rivers);
+
+}
+
+}
